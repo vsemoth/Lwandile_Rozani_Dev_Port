74942e7f72952564f54757ed001c071f13cfe0ef
vsemoth <vsemoth@gmail.com>
2021-07-25 14:32:19 +0200

    Modified blog owner detatils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I'm Lwandile Rozani.<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@vsemoth</span>
+          <h2>This is my Next.js website portfolio.</h2>
           <SocialList />
         </div>
       </div>
